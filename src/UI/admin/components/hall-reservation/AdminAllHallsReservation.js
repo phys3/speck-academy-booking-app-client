e3a70971400815b83bc69c0b7b76bc048d87bc64
Goran Ljaljić <gljaljic@gmail.com>
2019-07-07 20:01:53 +0200

    Minor changes

--- src/UI/admin/components/hall-reservation/AdminAllHallsReservation.js
+++ src/UI/admin/components/hall-reservation/AdminAllHallsReservation.js
@@ -13,43 +13,60 @@ import { API_BASE_URL } from '../../../../config';
 let startDay, endDay;
 let realData;
 
-function handledChange1(event) {
-  if (event.target.id === 'startDay') {
-    startDay = event.target.value;
-  } else if (event.target.id === 'endDay') {
-    endDay = event.target.value;
-  }
-  fetchIt(startDay, endDay);
-}
+//const AllHallsReservations = () => (
+class AllHallsReservations extends React.Component {
+  state = {
+    stateDataArray: dataArray
+  };
 
-function fetchIt(startDay, endDay) {
-  var url = new URL(API_BASE_URL + '/halls/reservations');
-  var params = { startDate: '2017-05-25', endDate: '2020-10-25' };
-  url.search = new URLSearchParams(params);
-  fetch(url, {
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json; charset=utf-8'
-    },
-    method: 'GET'
-  })
-    .then(response => response.json())
-    .then(data => (dataArray = data));
-}
+  fetchIt = (startDay, endDay) => {
+    console.log(this.state.stateDataArray);
+    var url = new URL(API_BASE_URL + '/halls/reservations');
+    var params = { startDate: '2017-05-25', endDate: '2020-10-25' };
+    url.search = new URLSearchParams(params);
+    fetch(url, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      method: 'GET'
+    }).then(response =>
+      this.setState({
+        stateDataArray: response.json()
+      })
+    );
+    //.then(response => response.json())
+    //.then(data => (this.setState({
+    //  stateDataArray: data
+    //})));
+  };
 
-const AllHallsReservations = () => (
-  <React.Fragment>
-    <DayPickerTitle>PREGLEDAJ REZERVACIJE ZA DANE:</DayPickerTitle>
-    <DayPicker1>
-      {
-        <form action="/action_page.php">
-          <input type="date" id="startDay" onChange={handledChange1} />
-          <input type="date" id="endDay" onChange={handledChange1} />
-          <input type="submit" />
-        </form>
-      }
-    </DayPicker1>
-    {/* <DayPicker2>
+  handledChange1 = event => {
+    if (event.target.id === 'startDay') {
+      startDay = event.target.value;
+    } else if (event.target.id === 'endDay') {
+      endDay = event.target.value;
+    }
+    this.fetchIt(startDay, endDay);
+  };
+
+  render() {
+    console.log('AAAAAAAAA');
+    console.log(this.state.stateDataArray);
+    console.log('BBBBBBBBB');
+    return (
+      <React.Fragment>
+        <DayPickerTitle>PREGLEDAJ REZERVACIJE ZA DANE:</DayPickerTitle>
+        <DayPicker1>
+          {
+            <form action="/action_page.php">
+              <input type="date" id="startDay" onChange={this.handledChange1} />
+              <input type="date" id="endDay" onChange={this.handledChange1} />
+              <input type="submit" />
+            </form>
+          }
+        </DayPicker1>
+        {/* <DayPicker2>
       {
         <form action="/action_page.php">
         <input type="date" name="year_week" />
@@ -58,11 +75,13 @@ const AllHallsReservations = () => (
       }
     </DayPicker2> */}
 
-    {dataArray.map(data => {
-      // {realData.map(data => {
-      return <ScheduleComponenet data={data} />;
-    })}
-  </React.Fragment>
-);
+        {dataArray.map(data => {
+          // {realData.map(data => {
+          return <ScheduleComponenet data={data} />;
+        })}
+      </React.Fragment>
+    );
+  }
+}
 
 export default AllHallsReservations;
